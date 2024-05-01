481c2a63a7f2b7c240ddfa7843ed903adf475cdb
jia-ayo <milkjia11@gmail.com>
2024-05-01 08:02:14 +0100

    begining -> end

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,11 @@ fn main() {
 
     let targets = vec![vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
     let mut network = Network::new(vec![2, 3, 1], 0.5, SIGMOID);
+    println!("0 and 0: {:?}", network.feed_forward(vec![0.0, 0.0]));
+    println!("1 and 0: {:?}", network.feed_forward(vec![1.0, 0.0]));
+    println!("0 and 1: {:?}", network.feed_forward(vec![0.0, 1.0]));
+    println!("1 and 1: {:?}", network.feed_forward(vec![1.0, 1.0]));
+
     network.train(inputs, targets, 10000);
 
     println!("0 and 0: {:?}", network.feed_forward(vec![0.0, 0.0]));
